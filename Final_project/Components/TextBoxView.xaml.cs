866da5259057fb8e8d2800e2881e03059438641a
teklit amanuel <teklitag@uia.no>
2024-04-13 23:14:20 +0200

    chage the report form to have its own textbox content container

---
+++ Final_project/Components/TextBoxView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Final_project.Components
+{
+    public partial class TextBoxView : UserControl
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text",
+            typeof(string),
+            typeof(TextBoxView),
+            new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty WatermarkProperty = DependencyProperty.Register(
+            "Watermark",
+            typeof(string),
+            typeof(TextBoxView),
+            new PropertyMetadata(default(string)));
+
+        public TextBoxView()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public string Watermark
+        {
+            get { return (string)GetValue(WatermarkProperty); }
+            set { SetValue(WatermarkProperty, value); }
+        }
+    }
+}
