3a4428cf82922f8954fdade3751a3df36346ac06
teklit amanuel <teklitag@uia.no>
2024-04-26 02:02:33 +0200

    style the window

--- Final_project/Views/MainWindow.xaml.cs
+++ Final_project/Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Windows;
-
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 namespace Final_project
 {
     public partial class MainWindow : Window
@@ -10,7 +11,20 @@ namespace Final_project
 
         }
 
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParm, int lparm);
 
+        private void panelcontrol_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void panelcontrol_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
     }
 
 }
