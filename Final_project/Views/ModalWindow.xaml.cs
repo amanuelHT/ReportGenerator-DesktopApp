f734dbd2b2a8a21edd20be7e90ce2fb3739bdb01
teklit amanuel <teklitag@uia.no>
2024-04-29 11:26:18 +0200

    fix some styling in home and fix main window bug style

--- Final_project/Views/ModalWindow.xaml.cs
+++ Final_project/Views/ModalWindow.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Interop;
 
 namespace Final_project.Views
 {
@@ -10,16 +11,46 @@ namespace Final_project.Views
             InitializeComponent();
         }
 
-        public void CloseWindow()
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParm, int lparm);
+
+        private void panelcontrol_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            try
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void panelcontrol_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+        private void closebtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement logic to close the window here (e.g., using Window.Close())
+            Window window = Window.GetWindow(this);
+            window.Close();
+        }
+
+        private void minimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement logic to minimize the window here (e.g., using Window.WindowState)
+            Window window = Window.GetWindow(this);
+            window.WindowState = WindowState.Minimized;
+        }
+
+        private void maximizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement logic to maximize/normalize the window here (e.g., using Window.WindowState)
+            Window window = Window.GetWindow(this);
+            if (window.WindowState == WindowState.Maximized)
             {
-                Close();
+                window.WindowState = WindowState.Normal;
             }
-            catch (Exception ex)
+            else
             {
-                // Log or handle the exception appropriately
-                Debug.WriteLine($"Exception occurred while closing the window: {ex.Message}");
+                window.WindowState = WindowState.Maximized;
             }
         }
 
