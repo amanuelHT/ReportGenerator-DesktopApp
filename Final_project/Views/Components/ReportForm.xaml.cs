5cfd6d6f0e38a6ff084191af274fba160cf849b9
teklit amanuel <teklitag@uia.no>
2024-05-05 19:38:02 +0200

    delete modalview

--- Final_project/Views/Components/ReportForm.xaml.cs
+++ Final_project/Views/Components/ReportForm.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace Final_project.Components
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Final_project.Components
 {
     /// <summary>
     /// Interaction logic for ReportForm.xaml
@@ -9,7 +13,35 @@
         {
             InitializeComponent();
         }
+        private void ChildControl_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
+            var eventArgs = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+            {
+                RoutedEvent = UIElement.MouseWheelEvent,
+                Source = sender
+            };
+            ((UIElement)sender).RaiseEvent(eventArgs);
+        }
 
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var scrollViewer = sender as ScrollViewer;
+            if (scrollViewer != null)
+            {
+
+                if (e.Delta < 0)
+                {
+                    scrollViewer.LineDown();
+                }
+                else
+                {
+                    scrollViewer.LineUp();
+                }
+
+                e.Handled = true;
+            }
+        }
 
     }
 }
